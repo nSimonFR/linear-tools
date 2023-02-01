eda0b0730c530cff9534c924e06bad1ef49690a6
Nicolas Simon <contact@nsimon.fr>
2023-02-01 13:19:11 +0100

    issue-move: allow cross-team move

--- issue-move.js
+++ issue-move.js
@@ -8,37 +8,41 @@ const linearClient = new LinearClient({
  * Moves a list of issues from one state to another
  * List elements can be attachment url or linear ids
  * If no from, will be automatically moved to to
-*/
-const issuesMove = async (list, to, from=null) => {
-  const teamQuery = { name: { eq: "Order Taking" } };
-
-  const statesResponse = await linearClient.client.rawRequest(`
-  query WorkflowStates($filter: WorkflowStateFilter) {
-    workflowStates(filter: $filter) {
-      nodes { id name position }
+ */
+const issuesMove = async (list, to, from) => {
+  const statesResponse = await linearClient.client.rawRequest(
+    `
+    query WorkflowStates($filter: WorkflowStateFilter) {
+      workflowStates(filter: $filter) {
+        nodes {
+          id
+          name
+          position
+          team {
+            id
+          }
+        }
+      }
+    }`,
+    {
+      filter: {
+        name: {
+          in: [from, to],
+        },
+      },
     }
-  }`,
-    { filter: { team: teamQuery } }
   );
 
   const states = statesResponse.data.workflowStates.nodes.sort(
     (a, b) => a.position - b.position
   );
 
-  const findState = (name) => {
-    const state = states.find((s) => s.name.toLowerCase() === name.toLowerCase());
-    if (!state) throw new Error(`Cannot find state ${name}.`);
-    return state;
-  }
+  const urls = list.filter((e) => e.includes("http"));
+  const ids = list.filter((e) => !e.includes("http"));
 
-  const fromState = from && findState(from);
-  const toState = findState(to);
-
-  const urls = list.filter(e => e.includes('http'));
-  const ids = list.filter(e => !e.includes('http'));
-
-  const issueQuery = `id url state { id }`
-  const response = await linearClient.client.rawRequest(`
+  const issueQuery = `id url state { id }`;
+  const response = await linearClient.client.rawRequest(
+    `
   query Attachments($attachmentFilter: AttachmentFilter, $issuesFilter: IssueFilter) {
     attachments(filter: $attachmentFilter) {
       nodes { issue { ${issueQuery} } }
@@ -46,22 +50,32 @@ const issuesMove = async (list, to, from=null) => {
     issues(filter: $issuesFilter) {
       nodes { ${issueQuery} }
     }
-  }`, {
-    attachmentFilter: { url: { in: urls } },
-    issuesFilter: { id: { in: ids } },
-  });
+  }`,
+    {
+      attachmentFilter: { url: { in: urls } },
+      issuesFilter: { id: { in: ids } },
+    }
+  );
 
   const issues = [
-    ...response.data.attachments.nodes.map(a => a.issue),
+    ...response.data.attachments.nodes.map((a) => a.issue),
     ...response.data.issues.nodes,
   ];
 
+  const findState = (name) =>
+    states.filter((s) => s.name.toLowerCase() === name.toLowerCase());
+
+  const froms = findState(from);
+  const tos = findState(to);
+
   const responses = await Promise.all(
     issues.map(async (issue) => {
-      if (fromState && issue.state.id !== fromState.id) return null;
+      const from = froms.find((f) => issue.state.id === f.id);
+      if (!from) return null;
 
+      const to = tos.find((t) => t.team.id === from.team.id);
       await linearClient.issueUpdate(issue.id, {
-        stateId: toState.id,
+        stateId: to.id,
       });
 
       return issue;
@@ -70,7 +84,7 @@ const issuesMove = async (list, to, from=null) => {
 
   const updatedIssues = responses.filter((d) => d);
 
-  updatedIssues.map((i) => console.log(i.id, i.url));
+  updatedIssues.map((i) => console.log(i.url));
 };
 
 const list = process.argv.slice(4);
